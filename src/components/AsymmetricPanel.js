9fbeabfe49c95cb0691be2224dfc7419ca238bc6
Nicolas Chartiot <nicolaschartiot@gmail.com>
2020-12-19 00:10:01 -0300

    Update material-ui imports

--- src/components/AsymmetricPanel.js
+++ src/components/AsymmetricPanel.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
+import { makeStyles, Tabs, Tab, Typography, Box } from '@material-ui/core';
 import Keys from './AsymmetricSteps/Keys';
 import Encryption from './AsymmetricSteps/Encryption';
 import Decryption from './AsymmetricSteps/Decryption';
--- src/components/AsymmetricSteps/Keys.js
+++ src/components/AsymmetricSteps/Keys.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import '../../style/Keys.css';
 
 const nacl = require('tweetnacl');
